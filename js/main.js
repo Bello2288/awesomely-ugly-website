c0b2fc11c0759e5cb3ea7207b1804a8bc7be3200
Bello2288 <bellofattogregg@gmail.com>
2022-08-30 14:47:09 -0400

    changed js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-function changeImage(){
-    const img = document.getElementById("images");
-    img.addEventListener('click', function(){
+// (function() {
 
-    img.scr = 'img\\uglypicture2';
-    })
+//     'use strict';
+
+const button = document.querySelector('button');
+const image = document.querySelector('.ugly-picture');
+
+button.addEventListener('click', changeImage);
+// image.style.backgroundImage = "url('images/up2.jpeg')"
+
+
+function changeImage() {
+  if (image.style.backgroundImage.value === "url('images/up1.jpeg')") {
+    image.style.backgroundImage = "url('images/up2.jpeg')";
+  } else {
+    image.style.backgroundImage = "url('images/up1.jpeg')"
+  }
 }
+
+// })();
